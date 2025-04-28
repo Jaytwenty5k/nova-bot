432fe9121dfa9bf7bb3256a3a2e938bc04f18529
Jaytwenty5k <JaydenWoppererYK@outlook.com>
2025-04-28 16:08:22 +0000

    sdfa

---
+++ src/app/avatar/upload/page.jsx
@@ -0,0 +1,8 @@
+export default function UploadPage() {
+  return (
+    <div>
+      <h1>Avatar Upload</h1>
+      {/* Stellen Sie sicher, dass die Seite korrekt gerendert wird */}
+    </div>
+  );
+}
